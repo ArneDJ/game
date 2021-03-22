287e5a6599199ad8c705bbe41a3aac691a7307a5
ArneDJ <ArneDeJonghe@protonmail.com>
2021-03-22 17:59:35 +0100

    removed crowd

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,6 @@
 #include "core/physics.h"
 #include "core/animation.h"
 #include "core/navigation.h"
-#include "crowd.h"
 #include "object.h"
 #include "debugger.h"
 //#include "core/sound.h" // TODO replace SDL_Mixer with OpenAL
@@ -70,11 +69,9 @@ private:
 	Timer timer;
 	Shader object_shader;
 	Shader debug_shader;
-	Shader creature_shader;
 	Camera camera;
 	Skybox *skybox;
 	Navigation navigation;
-	Crowd *crowd;
 	Debugger debugger;
 	struct {
 		uint16_t window_width;
@@ -83,7 +80,6 @@ private:
 		int FOV;
 		float look_sensitivity;
 	} settings;
-	std::vector<std::pair<DynamicObject*, const GLTF::Model*>> dynamics;
 	bool change_endpoint = false;
 	glm::vec3 endpoint = {};
 private:
@@ -138,10 +134,6 @@ void Game::init(void)
 	object_shader.compile("shaders/object.frag", GL_FRAGMENT_SHADER);
 	object_shader.link();
 
-	creature_shader.compile("shaders/creature.vert", GL_VERTEX_SHADER);
-	creature_shader.compile("shaders/creature.frag", GL_FRAGMENT_SHADER);
-	creature_shader.link();
-
 	camera.configure(0.1f, 9001.f, settings.window_width, settings.window_height, float(settings.FOV));
 	camera.position = { 10.f, 5.f, -10.f };
 	camera.lookat(glm::vec3(0.f, 0.f, 0.f));
@@ -154,65 +146,14 @@ void Game::init(void)
 	}
 }
 
-void Game::load_scene(void)
-{
-	GLTF::Model *sphere = new GLTF::Model { "media/models/sphere.glb", "" };
-	GLTF::Model *cone = new GLTF::Model { "media/models/cone.glb", "" };
-	GLTF::Model *capsule = new GLTF::Model { "media/models/capsule.glb", "" };
-	GLTF::Model *cylinder = new GLTF::Model { "media/models/cylinder.glb", "" };
-	GLTF::Model *cube = new GLTF::Model { "media/models/cube.glb", "media/textures/cube.dds" };
-
-	physicsman.add_ground_plane(glm::vec3(0.f, 0.f, 0.f));
-	DynamicObject *cube_ent = new DynamicObject {
-		glm::vec3(-10.f, 20.f, 10.f), glm::quat(1.f, 0.f, 0.f, 0.f), physicsman.add_box(glm::vec3(1.f, 1.f, 1.f))
-	};
-	DynamicObject *sphere_ent = new DynamicObject {
-		glm::vec3(-10.f, 22.f, 10.f), glm::quat(1.f, 0.f, 0.f, 0.f), physicsman.add_sphere(1.f)
-	};
-	DynamicObject *cone_ent = new DynamicObject {
-		glm::vec3(-10.f, 24.f, 10.f), glm::quat(1.f, 0.f, 0.f, 0.f), physicsman.add_cone(1.f, 2.f)
-	};
-	DynamicObject *capsule_ent = new DynamicObject {
-		glm::vec3(-10.f, 26.f, 10.f), glm::quat(1.f, 0.f, 0.f, 0.f), physicsman.add_capsule(0.5f, 1.f)
-	};
-	DynamicObject *cylinder_ent = new DynamicObject {
-		glm::vec3(-10.f, 30.f, 10.f), glm::quat(1.f, 0.f, 0.f, 0.f), physicsman.add_cylinder(glm::vec3(1.f, 1.f, 1.f))
-	};
-
-	physicsman.insert_body(cube_ent->body);
-	physicsman.insert_body(sphere_ent->body);
-	physicsman.insert_body(cone_ent->body);
-	physicsman.insert_body(capsule_ent->body);
-	physicsman.insert_body(cylinder_ent->body);
-
-	dynamics.push_back(std::make_pair(cube_ent, cube));
-	dynamics.push_back(std::make_pair(sphere_ent, sphere));
-	dynamics.push_back(std::make_pair(cone_ent, cone));
-	dynamics.push_back(std::make_pair(capsule_ent, capsule));
-	dynamics.push_back(std::make_pair(cylinder_ent, cylinder));
-}
-
 void Game::clear_scene(void)
 {
-	// first remove rigid bodies
-	for (const auto &dynamic : dynamics) {
-		physicsman.remove_body(dynamic.first->body);
-		// delete the rigid body
-		delete dynamic.first;
-		// delete the display model
-		delete dynamic.second;
-	}
-
-	dynamics.clear();
-
 	// then clear the physics manager
 	physicsman.clear();
 }
 
 void Game::teardown(void)
 {
-	delete crowd;
-
 	if (debugmode) {
 		debugger.teardown();
 	}
@@ -242,34 +183,12 @@ void Game::update(void)
 
 	physicsman.update(timer.delta);
 
-	for (auto &dynamic : dynamics) {
-		dynamic.first->update();
-	}
-
-	if (inputman.key_pressed(SDL_BUTTON_RIGHT) == true && inputman.mouse_grabbed() == false) {
-		glm::vec3 ray = camera.ndc_to_ray(inputman.abs_mousecoords());
-		struct ray_result result = physicsman.cast_ray(camera.position, camera.position + (1000.f * ray));
-		if (result.hit) {
-			change_endpoint = true;
-			endpoint = result.point;
-		}
-	}
-
-	inputman.update_keymap();
-		
-	if (change_endpoint) {
-		struct polyresult result = navigation.point_on_navmesh(endpoint);
-		if (result.found) {
-			crowd->retarget_agent(0, result.position, result.poly);
-		}
-	}
-
-	crowd->update(timer.delta);
-
 	if (debugmode) {
 		debugger.update(timer.ms_per_frame, camera.position);
 		if (debugger.exit_request) { running = false; }
 	}
+
+	inputman.update_keymap();
 }
 
 void Game::run(void)
@@ -278,99 +197,13 @@ void Game::run(void)
 	
 	debugger.add_grid(glm::vec2(-20.f, -20.f), glm::vec2(20.f, 20.f));
 
-	GLTF::Model building = { "media/models/building.glb", "" };
 	GLTF::Model duck = { "media/models/duck.glb", "media/textures/duck.dds" };
 	GLTF::Model dragon = { "media/models/dragon.glb", "" };
-	GLTF::Model toroid = { "media/models/toroid.glb", "" };
-	GLTF::Model monkey = { "media/models/monkey.glb", "" };
-	GLTF::Model cone = { "media/models/cone.glb", "" };
-	GLTF::Model human = { "media/models/fox.glb", "" };
-	GLTF::Model capsule = { "media/models/capsule.glb", "" };
-
-	btCollisionShape *shape = physicsman.add_box(glm::vec3(1.f, 1.f, 1.f));
-	for (const auto &mesh : toroid.collision_trimeshes) {
-		shape = physicsman.add_mesh(mesh.positions, mesh.indices);
-	}
-
-	StationaryObject stationary = { glm::vec3(-10.f, 5.f, 10.f), glm::quat(1.f, 0.f, 0.f, 0.f), shape };
-	for (const auto &mesh : building.collision_trimeshes) {
-		shape = physicsman.add_mesh(mesh.positions, mesh.indices);
-	}
-
-	glm::vec3 buildingpos = { -20.f, 3.f, -20.f };
-	StationaryObject building_ent = { buildingpos, glm::quat(1.f, 0.f, 0.f, 0.f), shape };
-
-	physicsman.insert_body(stationary.body);
-	physicsman.insert_body(building_ent.body);
-
-	btCollisionShape *hull = physicsman.add_box(glm::vec3(1.f, 1.f, 1.f));
-	for (const auto &hullmesh : monkey.collision_hulls) {
-		hull = physicsman.add_hull(hullmesh.points);
-	}
-	DynamicObject monkey_ent = { glm::vec3(-10.f, 30.f, 10.f), glm::quat(1.f, 0.f, 0.f, 0.f), hull };
-	physicsman.insert_body(monkey_ent.body);
-
-	load_scene();
-	
-	const std::vector<std::string> animationpaths = {
-		"media/animations/fox/idle.ozz",
-		//"media/animations/fox/walk.ozz",
-		"media/animations/fox/run.ozz"
-	};
-	Animator animator = { "media/skeletons/fox.ozz", animationpaths };
-	glm::mat4 creature_T = glm::translate(glm::mat4(1.f), glm::vec3(10.f, 0.f, -10.f));
-
-	TransformBuffer instancebuf;
-	instancebuf.matrices.resize(human.skins[0].inversebinds.size());
-	instancebuf.alloc(GL_DYNAMIC_DRAW);
-	instancebuf.update();
-
-	// create the navigation mesh
-	std::vector<float> vertex_soup;
-	std::vector<int> index_soup;
-	glm::mat4 building_T = glm::translate(glm::mat4(1.f), buildingpos);
-	//glm::mat4 R = glm::mat4(construct->rotation);
-	//glm::mat4 M = T * R;
-	glm::mat4 M = building_T;
-	int index_offset = vertex_soup.size()/3;
-	for (const auto &mesh : building.collision_trimeshes) {
-		for (const auto &pos : mesh.positions) {
-			glm::vec4 vertex = {pos.x, pos.y, pos.z, 1.f};
-			vertex = M * vertex;
-			vertex_soup.push_back(vertex.x);
-			vertex_soup.push_back(vertex.y);
-			vertex_soup.push_back(vertex.z);
-		}
-		for (const auto &index : mesh.indices) {
-			index_soup.push_back(index_offset + index);
-		}
-	}
-	if (navigation.build(vertex_soup, index_soup)) {
-		puts("succesfully built navigation mesh");
-	}
-
-	crowd = new Crowd { navigation.navmesh };
-	crowd->add_agent(glm::vec3(-14.f, 1.35f, -4.34f), endpoint, navigation.navquery);
-	
-	if (debugmode) {
-		debugger.add_navmesh(navigation.navmesh);
-	}
 
 	while (running) {
 		timer.begin();
 
 		update();
-		monkey_ent.update();
-
-		if (animator.is_valid()) {
-			animator.update(timer.delta);
-			for (const auto &skin : human.skins) {
-				for (int i = 0; i < animator.models.size(); i++) {
-					instancebuf.matrices[i] = ozz_to_mat4(animator.models[i]) * skin.inversebinds[i];
-				}
-			}
-			instancebuf.update();
-		}
 
 		renderman.prepare_to_render();
 
@@ -379,28 +212,6 @@ void Game::run(void)
 		debug_shader.uniform_mat4("VP", camera.VP);
 		debug_shader.uniform_mat4("MODEL", glm::scale(glm::mat4(1.f), glm::vec3(0.1f, 0.1f, 0.1f)));
 		dragon.display();
-
-		for (const auto &dynamic : dynamics) {
-			glm::mat4 T = glm::translate(glm::mat4(1.f), dynamic.first->position);
-			glm::mat4 R = glm::mat4(dynamic.first->rotation);
-			debug_shader.uniform_mat4("MODEL", T * R);
-			dynamic.second->display();
-		}
-		
-		debug_shader.uniform_mat4("MODEL", glm::translate(glm::mat4(1.f), stationary.position));
-		toroid.display();
-		debug_shader.uniform_mat4("MODEL", glm::translate(glm::mat4(1.f), monkey_ent.position) * glm::mat4(monkey_ent.rotation));
-		monkey.display();
-
-		glm::vec3 agent_pos = crowd->agent_position(0);
-		debug_shader.uniform_mat4("MODEL", glm::translate(glm::mat4(1.f), agent_pos));
-		capsule.display();
-
-		debug_shader.uniform_mat4("MODEL", building_T);
-		building.display();
-
-		debug_shader.uniform_mat4("MODEL", glm::translate(glm::mat4(1.f), endpoint));
-		cone.display();
 
 		if (debugmode) {
 			debug_shader.uniform_mat4("MODEL", glm::mat4(1.f));
@@ -415,12 +226,6 @@ void Game::run(void)
 		object_shader.uniform_mat4("MODEL", glm::translate(glm::mat4(1.f), glm::vec3(10.f, 0.f, 10.f)));
 		duck.display();
 
-		creature_shader.use();
-		creature_shader.uniform_mat4("VP", camera.VP);
-		creature_shader.uniform_mat4("MODEL", creature_T);
-		instancebuf.bind(GL_TEXTURE10);
-		human.display();
-
 		skybox->display(&camera);
 
 		if (debugmode) {
@@ -431,10 +236,6 @@ void Game::run(void)
 		timer.end();
 	}
 
-	delete crowd;
-	physicsman.remove_body(stationary.body);
-	physicsman.remove_body(building_ent.body);
-	physicsman.remove_body(monkey_ent.body);
 	clear_scene();
 }
 
